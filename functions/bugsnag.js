71c4617aa6aa564b582390f6e27dc679862ec5e4
horimislime <horimislime@gmail.com>
2017-09-13 01:28:19 +0900

    Split files

--- functions/bugsnag.js
+++ functions/bugsnag.js
@@ -3,34 +3,7 @@
 const request = require('request');
 require('dotenv').config();
 
-Object.defineProperty(Array.prototype, 'group', {
-  enumerable: false,
-  value: (key) => {
-    let map = {};
-    this.map(e => ({k: key(e), d: e})).forEach(e => {
-      map[e.k] = map[e.k] || [];
-      map[e.k].push(e.d);
-    });
-    return Object.keys(map).map((k) => ({key: k, data: map[k]}));
-  }
-});
-
-const filterParams = {
-  'filters[event.since][][type]': 'eq',
-  'filters[event.since][][value]': '2d',
-  'filters[error.status][][type]': 'eq',
-  'filters[error.status][][value]': 'open',
-  'filters[event.severity][][type]': 'eq',
-  'filters[event.severity][][value]': 'error',
-  'filters[app.release_stage][][type]': 'eq',
-  'filters[app.release_stage][][value]': 'production'
-};
-
-const flatten = (arr) => {
-  return Array.prototype.concat(...arr);
-};
-
-const api = (url, params) => {
+module.exports = (url, params) => {
   return new Promise((resolve, reject) => {
     request.get({
       url: url,
@@ -50,24 +23,3 @@ const api = (url, params) => {
     });
   });
 };
-
-api(`https://api.bugsnag.com/projects/${process.env.PROJECT_ID}/errors`, filterParams)
-  .then((errors) => {
-    const req = errors.map((error) => api(`https://api.bugsnag.com/errors/${error.id}/events`));
-    return Promise.all(req);
-  })
-  .then((events) => {
-    const req = flatten(events).map((event) => api(event.url));
-    return Promise.all(req);
-  })
-  .then((details) => {
-    details.forEach((detail) => {
-      console.log(detail.user.name);
-    });
-
-    const restaurantGroups = details.group((detail) => detail.user.id);
-    const versionGroups = details.group((detail) => detail.app.version);
-
-    console.log(`restaurant group count = ${restaurantGroups.length}`);
-    console.log(`version group count = ${versionGroups.length}`);
-  });
--- functions/index.js
+++ functions/index.js
@@ -12,9 +12,63 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var functions = require('firebase-functions');
+// var functions = require('firebase-functions');
+//
+// exports.hourly_job =
+//   functions.pubsub.topic('hourly-tick').onPublish((event) => {
+//     console.log("This job is ran every hour!")
+//   });
 
-exports.hourly_job =
-  functions.pubsub.topic('hourly-tick').onPublish((event) => {
-    console.log("This job is ran every hour!")
+'use strict';
+
+const _ = require('lodash');
+const api = require('./bugsnag.js');
+require('dotenv').config();
+
+Object.defineProperty(Array.prototype, 'group', {
+  enumerable: false,
+  value: (key) => {
+    let map = {};
+    this.map(e => ({k: key(e), d: e})).forEach(e => {
+      map[e.k] = map[e.k] || [];
+      map[e.k].push(e.d);
+    });
+    return Object.keys(map).map((k) => ({key: k, data: map[k]}));
+  }
+});
+
+const flatten = (arr) => {
+  return Array.prototype.concat(...arr);
+};
+
+const filterParams = {
+  'filters[event.since][][type]': 'eq',
+  'filters[event.since][][value]': '2d',
+  'filters[error.status][][type]': 'eq',
+  'filters[error.status][][value]': 'open',
+  'filters[event.severity][][type]': 'eq',
+  'filters[event.severity][][value]': 'error',
+  'filters[app.release_stage][][type]': 'eq',
+  'filters[app.release_stage][][value]': 'production'
+};
+
+api(`https://api.bugsnag.com/projects/${process.env.PROJECT_ID}/errors`, filterParams)
+  .then((errors) => {
+    const req = errors.map((error) => api(`https://api.bugsnag.com/errors/${error.id}/events`));
+    return Promise.all(req);
+  })
+  .then((events) => {
+    const req = flatten(events).map((event) => api(event.url));
+    return Promise.all(req);
+  })
+  .then((details) => {
+    details.forEach((detail) => {
+      console.log(detail.user.name);
+    });
+
+    const restaurantGroups = details.group((detail) => detail.user.id);
+    const versionGroups = details.group((detail) => detail.app.version);
+
+    console.log(`restaurant group count = ${restaurantGroups.length}`);
+    console.log(`version group count = ${versionGroups.length}`);
   });
