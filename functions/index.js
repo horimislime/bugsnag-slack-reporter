5bc12fbb9981ae3cb4513e0707f8b453c7a390ea
horimislime <horimislime@gmail.com>
2017-09-13 15:01:41 +0900

    Customize message body

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,14 @@
-// Copyright 2017 Google Inc. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// var functions = require('firebase-functions');
-//
-// exports.hourly_job =
-//   functions.pubsub.topic('hourly-tick').onPublish((event) => {
-//     console.log("This job is ran every hour!")
-//   });
-
 'use strict';
 
 const _ = require('lodash');
+const functions = require('firebase-functions');
 const bugsnag = require('./bugsnag.js');
 const slack = require('./slack.js');
 require('dotenv').config();
 
 const filters = {
   'filters[event.since][][type]': 'eq',
-  'filters[event.since][][value]': '2d',
+  'filters[event.since][][value]': '3d',
   'filters[error.status][][type]': 'eq',
   'filters[error.status][][value]': 'open',
   'filters[event.severity][][type]': 'eq',
@@ -37,39 +17,50 @@ const filters = {
   'filters[app.release_stage][][value]': 'production'
 };
 
-bugsnag.errors(process.env.PROJECT_ID, filters)
-  .then((errors) => {
-    const req = errors.map((error) => bugsnag.events(error.id, filters));
-    return Promise.all(req);
-  })
-  .then((events) => {
-    const req = _.flatten(events).map((event) => bugsnag.get(event.url));
-    return Promise.all(req);
-  })
-  .then((details) => {
-    const userGroups = _.groupBy(details, (detail) => detail.user.name);
-    const versionGroups = _.groupBy(details, (detail) => detail.app.version);
+exports.daily_job = functions.pubsub.topic('daily-tick').onPublish(() => {
+  console.log('Job started.');
+  bugsnag.errors(process.env.PROJECT_ID, filters)
+    .then((errors) => {
+      const req = errors.map((error) => bugsnag.events(error.id, filters));
+      return Promise.all(req);
+    })
+    .then((events) => {
+      const req = _.flatten(events).map((event) => bugsnag.get(event.url));
+      return Promise.all(req);
+    })
+    .then((details) => {
+      const userGroups = _.groupBy(details, (detail) => detail.user.name);
+      const versionGroups = _.groupBy(details, (detail) => detail.app.version);
 
-    userGroups['ほげレストラン'] = [1, 2, 3, 4, 5, 6, 7];
-    userGroups['居酒屋ぽめ太郎'] = [1, 2, 3, 4, 5];
+      let message = ':fire: Total number of crashes on yesterday :fire:\n\n\n';
 
-    const users = _.sortBy(_.keys(userGroups), (user) => userGroups[user].length)
-      .reverse()
-      .map((user) => `${user}: ${userGroups[user].length}`)
-      .join('\n');
-    slack.send(`*Crashes by user* \`\`\`${users}\`\`\``);
+      const users = _.sortBy(_.keys(userGroups), (user) => userGroups[user].length).reverse();
+      message += ':cat: *Crashes by user*\n';
+      if (users.length > 0) {
+        const usersString = users.map((user) => `${user}: ${userGroups[user].length}`).join('\n');
+        message += `\`\`\`${usersString}\`\`\``;
+      } else {
+        message += 'No crashes :tada:';
+      }
+      message += '\n\n\n';
+      message += ':rocket: *Crashes by version*\n';
 
-    versionGroups['13.0'] = [1, 2, 3, 4, 5, 6, 7];
-    versionGroups['14.0'] = [1, 2, 3, 4, 5];
-    const versions = _.sortBy(_.keys(versionGroups), (ver) => parseFloat(ver))
-      .reverse()
-      .map((ver) => `${ver}: ${versionGroups[ver].length}`)
-      .join('\n');
-    slack.send(`*Crashes by version* \`\`\`${versions}\`\`\``);
+      const versions = _.sortBy(_.keys(versionGroups), (ver) => parseFloat(ver)).reverse();
+      if (versions.length > 0) {
+        const versionsString = versions.map((ver) => `${ver}: ${versionGroups[ver].length}`).join('\n');
+        const total = _.flatten(_.toArray(versionGroups)).length;
+        message += ` \`\`\`${versionsString}\`\`\`\n\n`;
+        message += `Total: ${total}`;
+      } else {
+        message += 'No crashes :tada:';
+      }
+      message += '\n\n\n';
 
-    bugsnag.project(process.env.PROJECT_ID)
-      .then((project) => {
-        const filterParam = _.toPairs(filters).map((pair) => pair.join('=')).join('&');
-        slack.send(`For more information:\n${project.html_url}/errors?${filterParam}`);
-      });
-  });
+      bugsnag.project(process.env.PROJECT_ID)
+        .then((project) => {
+          const filterParam = _.toPairs(filters).map((pair) => pair.join('=')).join('&');
+          message += `:sleuth_or_spy: *More detail*\n${project.html_url}/errors?${filterParam}`;
+          slack.send(message);
+        });
+    });
+});
